bd486861e92d829d45350887c8d28aa70ab23f56
aruyamam <aruyamam@gmail.com>
2018-06-18 15:39:16 +0900

    DOM Building

--- 19/app.js
+++ 19/app.js
@@ -27,4 +27,12 @@ function updateState(state, action) {
   return Object.assign({}, state, action);
 }
 
-
+function elt(type, props, ...children) {
+  let dom = document.createElement(type);
+  if (props) Object.assign(dom, props);
+  for (let child of children) {
+    if (typeof child !== 'string') dom.appendChild(child);
+    else dom.appendChild(document.createTextNode(child));
+  }
+  return dom;
+}
